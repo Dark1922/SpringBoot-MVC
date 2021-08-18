781afb1b9401c096c29c73e4da92bee3b85f10ee
dark1922 <jvdematos004@gmail.com>
2021-08-18 20:16:58 -0300

    feath: editando pessoa

--- curso.springboot/src/main/java/curso/springboot/controller/PessoaController.java
+++ curso.springboot/src/main/java/curso/springboot/controller/PessoaController.java
@@ -1,8 +1,12 @@
 package curso.springboot.controller;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -19,12 +23,19 @@ public class PessoaController {
 	private PessoaRepository pessoaRepository;
 
 	@RequestMapping(method = RequestMethod.GET, value = "/cadastropessoa")
-	public String inicio() {
+	public ModelAndView inicio() {
+		
+		ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
+		
+		//retorna uma pessoa vázia pra poder aparecer a tela normalmente
+		modelAndView.addObject("pessoaobj", new Pessoa());
 
-		return "cadastro/cadastropessoa";
+		return modelAndView;
 	}
 
-	@PostMapping(value = "/salvarpessoa")
+	// **/salvarpessoa ele intercepta salvar pessoa de qualquer forma
+	//ignora tudo que venha antes e pegue a pessoa que quer salvar
+	@PostMapping(value = "**/salvarpessoa")
 	public ModelAndView salvar(Pessoa pessoa) {
 
 		pessoaRepository.save(pessoa);
@@ -36,6 +47,9 @@ public class PessoaController {
 
 		// para o objeto de pessoas joga a lista pramim
 		modelAndView.addObject("pessoas", pessoasIterator);
+		
+		//objeto vazio pro formulário trabalhar corretamente
+		modelAndView.addObject("pessoaobj", new Pessoa());
 
 		return  modelAndView;
 	}
@@ -44,13 +58,32 @@ public class PessoaController {
 	public ModelAndView pessoas() {
 
 		ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
-
+		
 		// retorna iterable por isso n vai por uma lista
 		Iterable<Pessoa> pessoasIterator = pessoaRepository.findAll();
 
 		// para o objeto de pessoas joga a lista pramim
 		modelAndView.addObject("pessoas", pessoasIterator);
+		
+		//objeto vazio pro formulário trabalhar corretamente
+		modelAndView.addObject("pessoaobj", new Pessoa());
 
+		return modelAndView;
+	}
+	//pega na url o id pessoa
+	@GetMapping("/editarpessoa/{idpessoa}")
+	public ModelAndView	editar(@PathVariable("idpessoa") Long idpessoa) {
+		
+		//vai buscar o id da pessoa carregar o objeto do banco de dados consultou
+		Optional<Pessoa> pessoa = pessoaRepository.findById(idpessoa);
+		
+		//vai retorna pra mesma tela de cadastro o retorno
+		ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
+		
+		//vai addicionar o objeto pessoa com get que vai buscar seus dados pra edição
+		modelAndView.addObject("pessoaobj", pessoa.get());
+		
+		
 		return modelAndView;
 	}
 }
