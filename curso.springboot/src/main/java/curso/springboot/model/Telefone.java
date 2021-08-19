a5996916073364838b1dea336d840298007d448f
dark1922 <jvdematos004@gmail.com>
2021-08-19 18:35:31 -0300

    feath: relacionamento um para muitos de pessoa com telefones

--- curso.springboot/src/main/java/curso/springboot/model/Telefone.java
+++ curso.springboot/src/main/java/curso/springboot/model/Telefone.java
@@ -1,14 +1,15 @@
 package curso.springboot.model;
 
-
 import java.io.Serializable;
-import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.ForeignKey;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -16,22 +17,19 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-public class Pessoa implements Serializable {
-
+public class Telefone implements Serializable {
+	
 	private static final long serialVersionUID = 1L;
-
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long id;
+    private Long id;
 	
-	private String nome;
+	private String numero;
 	
-	private String sobrenome;
+	private String tipo;
 	
-	private Integer idade;
-	
-	@OneToMany(mappedBy = "pessoa") //referencia o nome do relacionamento da outra classe
-	private List<Telefone> telefones;
-	
-
+	@ManyToOne
+	@ForeignKey(name = "pessoa_id")
+	private Pessoa pessoa;
 }
