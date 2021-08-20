4b38a649c70dd65f4093f3f4a4c264fbacb1dea6
dark1922 <jvdematos004@gmail.com>
2021-08-20 17:38:28 -0300

    feath: testando criptografia de senha na configuração spring security

--- curso.springboot/src/main/java/curso/springboot/security/WebConfigSecurity.java
+++ curso.springboot/src/main/java/curso/springboot/security/WebConfigSecurity.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -32,9 +33,9 @@ public class WebConfigSecurity extends WebSecurityConfigurerAdapter {
 	@Override //Cria Autenticação do Usuário com banco de dados em memória
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		
-		auth.inMemoryAuthentication().passwordEncoder(NoOpPasswordEncoder.getInstance())
+		auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder())
 		.withUser("admin") //usuario que eu quero
-		.password("admim")
+		.password("$2a$10$/bakAy1Gm5EKtCOTMNWxg.jppKIYV.sp659l5NqWHw66OWxyOYxLm")
 		.roles("ADMIN");
 	}
 	
