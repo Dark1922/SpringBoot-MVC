3a83726a72e81ecb9cb5a720376ccf84316a3847
dark1922 <jvdematos004@gmail.com>
2021-08-24 23:22:35 -0300

    sexo

--- curso.springboot/src/main/java/curso/springboot/IndexController.java
+++ curso.springboot/src/main/java/curso/springboot/IndexController.java
@@ -1,4 +1,4 @@
-package curso.springboot.controller;
+package curso.springboot;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
 
 	@RequestMapping("/")
-	public String login() {
+	public String login() { 
 		return "login";
 	}
 
